d6c50efc73d5f95d157eb79292f282e7aef589f4
DirarSweidan <dirar.sweidan.research@gmail.com>
2024-08-21 20:43:51 +0200

    Update OrthogonalizationMethod.py

--- MultiViewClusteringViaOrthogonalization/OrthogonalizationMethod.py
+++ MultiViewClusteringViaOrthogonalization/OrthogonalizationMethod.py
@@ -9,8 +9,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
 from scipy.linalg import fractional_matrix_power
-import os
-os.environ['OMP_NUM_THREADS'] = '2'
+
 
 # ========================================================================
 
@@ -41,7 +40,7 @@ def data432():								# 4 features, 3 clusters, 2 views
 	S  = [200, 200, 100]
 	
 	for i in range(len(S)):
-		X2 += np.random.multivariate_normal(M[i], np.identity(2)/3, soze=S[i]).tolist()
+		X2 += np.random.multivariate_normal(M[i], np.identity(2)/3, size=S[i]).tolist()
 	
 	X  = np.array([ X1[i] + X2[i] for i in range(len(X1)) ]) # Combine the two views (four features F1, F2, F3, F4)
 	X  = X - X.mean(axis=0)					# Center the data (shift the data points toward the origine)
@@ -56,21 +55,21 @@ def plot_clusters(DATA, X, colors):
 	ax1a.set_ylabel('Feature 2')
 	
 	if len(DATA[0]) > 2:
-		ax1a.scatter( *np.array([ *zip(*DATA) ])[2:4], c=colors, marker='.' )
-		ax1a.set_title('Original Space')
-		ax1a.set_xlabel('Feature 3')
-		ax1a.set_ylabel('Feature 4')
+		ax2a.scatter( *np.array([ *zip(*DATA) ])[2:4], c=colors, marker='.' )
+		ax2a.set_title('Original Space')
+		ax2a.set_xlabel('Feature 3')
+		ax2a.set_ylabel('Feature 4')
 		
-	ax1b.scatter( *np.array([ *zip(*DATA) ])[:2], c=colors, marker='.' )
+	ax1b.scatter( *np.array([ *zip(*X) ])[:2], c=colors, marker='.' )
 	ax1b.set_title('Transformed Space')
 	ax1b.set_xlabel('Feature 1')
 	ax1b.set_ylabel('Feature 2')
 	
 	if len(DATA[0]) > 2:
-		ax1b.scatter( *np.array([ *zip(*DATA) ])[2:4], c=colors, marker='.' )
-		ax1b.set_title('Transformed Space')
-		ax1b.set_xlabel('Feature 3')
-		ax1b.set_ylabel('Feature 4')
+		ax2b.scatter( *np.array([ *zip(*X) ])[2:4], c=colors, marker='.' )
+		ax2b.set_title('Transformed Space')
+		ax2b.set_xlabel('Feature 3')
+		ax2b.set_ylabel('Feature 4')
 	
 	plt.show()
 	
@@ -99,10 +98,11 @@ def mView_Clustering_via_Orthogonalization(DATA, alternatives):
 # ========================================================================
 # Paper: Y. Cui et al. (2007). Non-redundant multi-view clustering via orthogonalization. ICDM (pp. 133-142).
 
+#k    = 2
+#DATA = generate_data(type='2-2-4')
 
-k = 2
+k    = 3
+DATA = generate_data(type='4-3-2')
+
 alternatives = 5
-
-DATA = generate_data(type='2-2-4')
-#DATA = generate_data(type='4-3-2')
 mView_Clustering_via_Orthogonalization(DATA, alternatives)
