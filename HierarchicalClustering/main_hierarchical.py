62d2677cb6727ffc3f1d4599dddf48ccd39fdc58
DirarSweidan <dirar.sweidan.research@gmail.com>
2024-09-09 15:52:24 +0200

    Update main_hierarchical.py

--- HierarchicalClustering/main_hierarchical.py
+++ HierarchicalClustering/main_hierarchical.py
@@ -7,16 +7,20 @@
 This function returns a hierarchical clustering class model that containes model.clutserings, model.linkagematrix, model.labels, and model.centers 
 '''
 
-import random
-import copy
+from itertools import combinations
 import numpy as np
-import matplotlib.pyplot as plt
-from sklearn.cluster import KMeans
-from scipy.linalg import fractional_matrix_power
+import math
+import copyfrom scipy.cluster.hierarchy import dendrogram
+
+#import matplotlib.pyplot as plt
+#from sklearn.cluster import KMeans
+#from scipy.linalg import fractional_matrix_power
 
 
 # ========================================================================
 
 def hierarchical(DATA, n_clusters=2, linkage='average', affinity='euclidean'):
+	def euc(a,b): return math.sqrt( sum( [ [a[i] - b[i]]** for i in range(len(a))] ) )
+	def man(a,b): return sum(       abs( [ [a[i] - b[i]]** for i in range(len(a))] ) ) 
 	
 
