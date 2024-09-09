01082ddd6dd2b9114286d27b32a641251b2a7dd3
DirarSweidan <dirar.sweidan.research@gmail.com>
2024-09-09 15:45:49 +0200

    Update main_hierarchical.py

--- HierarchicalClustering/main_hierarchical.py
+++ HierarchicalClustering/main_hierarchical.py
@@ -3,6 +3,10 @@
 # Authors: DirarSweidan
 # License: DSB 3-Claus
 
+'''
+This function returns a hierarchical clustering class model that containes model.clutserings, model.linkagematrix, model.labels, and model.centers 
+'''
+
 import random
 import copy
 import numpy as np
@@ -12,4 +16,7 @@ from scipy.linalg import fractional_matrix_power
 
 
 # ========================================================================
+
+def hierarchical(DATA, n_clusters=2, linkage='average', affinity='euclidean'):
+	
 
